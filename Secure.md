2506663a0bcbd547f6cea581460cf951ccdad611
daboynb <106079917+daboynb@users.noreply.github.com>
2022-07-01 13:44:07 +0200

    Add files via upload

---
+++ Secure.md
@@ -0,0 +1,76 @@
+# Pre-requisistes
+
+1) You'll need a pc with: uefi, secure boot and tpm enabled.</br>
+ 
+2) Bios </br>
+Set a power-on and bios password, they can be easily bypassed (see bios-pw.org) but if someone will bypass them with a master key, at the next boot you will notice the absence of the prompt that ask for the password.
+
+3) Encryption</br>
+Use veracrypt for windows and luks for linux. To make a dual boot, first install windows.
+
+# Install Windows</br>
+- we need to create two partitions, one for efi and one for c.
+- during the install, when you'll arrive at the partitioning menu delete all partitions and make all the space unallocated
+- press shift and f10
+- on cmd type: diskpart
+- on diskpart type: list disk (to see all disks)
+- on diskpart type: sel disk (the one where you want to install windows)
+- on diskpart type : convert gpt (########### if you have mbr partition table ###########)
+- on diskpart type: create partition efi size=500 
+- on diskpart type: create partition primary size=xx the remaining disk size in mb
+- close the cmd, select the c drive and tap install
+
+# Post install </br>
+This is a prsonal thing that I do, I don't like to have a windows recovery partition, instead I use a bootable usb if I need it.
+Your choiche to follow or not. Less unencrypted partition we have, less are the probabilities to have problems.
+- open the disks utility, there's will be a third partition
+- open diskpart as admin
+- on diskpart type: list disk (to see all disks)
+- on diskpart type: select disk x (where x is the number of the disk where windows is installed)
+- on diskpart type: list partition (to deect wich one is the third created)
+- on diskpart type: select partition x (where x is the number of the recovery partition grabbed from the command list partition)
+- on diskpart type: delete partition override
+- now if all goes well you'll remain wih only two partitions, c and efi
+- install veracrypt, chose encrypt the windows system partition and complete the setup
+
+# Install Linux</br>
+- boot from the usb your distro and chose install 
+- on partition menu : 
+  -  assign the previously created /efi partition as efi 
+  -  create an ext4 partition of 1gb and select as mount point /boot
+  -  create an ext4 partition with / as mount point and configure it as a volume for physical encryption.
+Personally I don't use a swap partition, instead I use a swap file. Your choiche. 
+
+# Post install</br>
+- install grub-customizer
+- open the windows entry and edit it: change /EFI/Microsoft/Boot/bootmgfw.efi to /EFI/VeraCrypt/DcsBoot.efi
+- save the configuration and exit, at reboot you'll be able to choose linux or windows
+- on terminal type: sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="/&net.ifnames=0 biosdevname=0 /' /etc/default/grub  && sudo update-grub
+
+# Consideration</br>
+So now we still have two unencrypted partitions, /boot and /efi.</br>
+My workarounds.</br>
+
+  1) The shasum of the partitions dump change every reboot, so I've decided to check the sha512 of every file in the /boot and /efi partition and compare them every startup. Note -> if you start windows, the script will detect the changes in the windows entries, I personally want it but if you don't; change the first check to exclude the Microsoft folder inside the /efi prtition. </br>
+</br>
+    With that, if someone will do an evil maid attack we'll notice it and since the script is configured to not power on internet unitil all checks are passed, the grabbed password will not be transmitted. There're other things to consider.... Two at least: uefi malware and ssd or hdd with a malware in the firmware. So here's my effort trying to mitigate the impossible.
+</br>
+</br>
+   1) check all the ssd infos like Model Number, Serial Number and Firmware Revision etc...</br>
+   2) check bios infos like Version, Release Date, Runtime Size, ROM Size etc...</br>
+   3) check tpm pcr_0 that is where bios and extensions are located and pcr_8 where tpm store the hash on initrd.</br>
+
+# Extras
+This script do other things too:
+  - update the system
+  - change the mac address 
+  - disable internet at boot and enable it only when all checks are passed</br>
+  
+ The commands that update the system and install the dependencies are for debian based distros, if you have something else replace them with your package manager.
+
+# Running
+Download the script and run it, it will install the dependencies and create the hashes. Then cnfigure it to run on startup (on gnome put the check.desktop inside  ~/.config/autostart and change it like you need) Enjoy.
+
+
+# Credits for tpm dump
+https://github.com/9elements/tpmtool for the extraction of the tpm hashes
---
+++ check.sh
@@ -0,0 +1,234 @@
+#!/bin/bash
+<<comment
+To run at startup and enable the old network interfaces: 
+- download check.desktop, edit with your username and and copy into ~/.config/autostart  
+- sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="/&net.ifnames=0 biosdevname=0 /' /etc/default/grub  && sudo update-grub                                                                                           
+comment
+#------------------------------------------Pre-steps---------------------------------------------------------------#
+# Enable leds
+xset led 3
+
+# Ask for sudo privileges
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+
+# Check requirements
+sudo dpkg -l | grep -qw macchanger || sudo apt-get install macchanger -y
+sudo dpkg -l | grep -qw figlet || sudo apt-get install figlet -y
+sudo dpkg -l | grep -qw dmidecode || sudo apt-get install dmidecode -y
+sudo dpkg -l | grep -qw lshw || sudo apt-get install lshw -y
+
+tpm=$(sudo dpkg -l | grep -E  tpmtool)
+
+if [[ -z "$tpm" ]]; then
+    wget https://github.com/9elements/tpmtool/releases/download/v3/tpmtool_3_linux_amd64.deb
+    sudo dpkg -i tpmtool_3_linux_amd64.deb
+    sudo apt-get install -f -y
+    sudo rm tpmtool_3_linux_amd64.deb
+    else
+    echo "Tpmtool alreay installed"
+fi
+
+#------------------------------------------Variables declaration---------------------------------------------------------------#
+echo "////////////////////////////////////////////////////////////////////////////"
+
+# Partition variables, change like /dev/sdax where x is number of the desired partition
+efipartition=/dev/sda1
+
+#Ssd variable
+ssd=/dev/sda
+
+# Network interfaces variables, change like you need
+ethernet=eth0
+wireless=wlan0
+
+#------------------------------------------Check for previous txt---------------------------------------------------------------#
+echo "Checking folders and files"
+
+# Check if the working directory exist otherwise it will create one
+ if [ -d /sec ]  
+ then
+    echo "/sec exist"
+    cd "/sec" || echo "Can't access to /sec" || exit
+ else
+    echo "/sec not exist"
+    mkdir "/sec"
+    cd "/sec" || echo "Can't access to /sec" || exit
+ fi
+
+# Check if the original list of sha512sums of the /boot and /efi partitions is present otherwise it will create one
+ if [ -f /sec/sha512sum_list_boot_orig.txt ]  
+ then
+     echo "sha512sum_list_orig exist"
+ else
+    echo "List sha512sum of /boot and /efi partition"
+    sudo find /boot -type f -exec sha512sum "{}" + > ./sha512sum_list_boot_orig.txt
+fi
+
+ # Check if the orginal file containing the bios infos is present otherwise it will create one
+  if [ -f /sec/bios_info_orig.txt ]  
+ then
+     echo "bios_info_orig exist"
+ else
+   echo "Extract bios infos"
+   sudo dmidecode --type bios > ./bios_info_orig.txt
+   sudo tail -n +2 '/sec/bios_info_orig.txt' > temp.tmp && sudo mv temp.tmp '/sec/bios_info_orig.txt'
+ fi
+
+ # Check if the orginal file containing the ssd infos is present otherwise it will create one
+  if [ -f /sec/ssd_orig.txt ]  
+ then
+     echo "ssd_orig exist"
+ else
+   echo "Extract ssd infos"
+   sudo lshw -class disk | grep /dev/sda -A 5 -B 5 > ./ssd_orig.txt
+ fi
+
+ # Check if the orginal file containing the tpm_pcr_0 hashes is present otherwise it will create one
+  if [ -f /sec/tpm.bootguard_orig.txt ]  
+ then
+     echo "tpm.bootguard_orig exist"
+ else
+   echo "Extract tpm infos"
+   sudo tpmtool eventlog dump | grep -A 3 'PCR: 0' > ./tpm.bootguard_orig.txt
+ fi
+ # Check if the orginal file containing the tpm_pcr_8 hashes is present otherwise it will create one
+  if [ -f /sec/tpm.kernel_orig.txt ]  
+ then
+     echo "tpm.kernel_orig exist"
+ else
+   echo "Extract tpm infos"
+   sudo tpmtool eventlog dump | grep -E -A 1 -B 2 "initrd /" > ./tpm.kernel_orig.txt
+ fi
+#------------------------------------------Compare old to new---------------------------------------------------------------#
+echo "////////////////////////////////////////////////////////////////////////////"
+# Calculate sha512sum of the /boot and /efi partitions files and compare the new txt with the old one
+echo "Checking for / boot and /efi partition changes"
+sudo find /boot -type f -exec sha512sum "{}" + > ./sha512sum_list_boot_new.txt
+if diff -s ./sha512sum_list_boot_orig.txt ./sha512sum_list_boot_new.txt
+then
+    figlet "/BOOT & /EFI MATCH"
+    sudo rm -rf /sec/sha512sum_list_boot_new.txt
+else
+    read -r -p "DATA GOT TAMPERED, PRESS ENTER TO CONTINUE AND INGORE IT. If you have booted windows is totally normal that it's entries are different"
+fi
+
+echo "////////////////////////////////////////////////////////////////////////////"
+# Extract the bios infos and compare them to the old txt
+echo "Extract bios infos"
+sudo dmidecode --type bios > ./bios_info_new.txt
+sudo tail -n +2 '/sec/bios_info_new.txt' > temp.tmp && sudo mv temp.tmp '/sec/bios_info_new.txt'
+if diff -s bios_info_orig.txt bios_info_new.txt
+then
+    figlet BIOS MATCH
+    sudo rm -rf /sec/bios_info_new.txt
+else
+    read -r -p "DATA GOT TAMPERED, PRESS ENTER TO CONTINUE AND INGORE IT"
+fi
+
+echo "////////////////////////////////////////////////////////////////////////////"
+# Extract the ssd infos and compare them to the old txt
+echo "Extract ssd infos"
+sudo lshw -class disk | grep /dev/sda -A 5 -B 5 > ./ssd_new.txt
+if diff -s ssd_orig.txt ssd_new.txt
+then
+    figlet SSD MATCH
+    sudo rm -rf /sec/ssd_new.txt
+else
+    read -r -p "DATA GOT TAMPERED, PRESS ENTER TO CONTINUE AND INGORE IT"
+fi
+
+echo "////////////////////////////////////////////////////////////////////////////"
+# Extract the tpm_pcr_0 hashes and compare them to the old txt
+echo "Extract tpm infos"
+sudo tpmtool eventlog dump | grep -A 3 'PCR: 0' > ./tpm.bootguard_new.txt 
+
+if diff -s tpm.bootguard_orig.txt tpm.bootguard_new.txt 
+then
+    figlet PCR 0 MATCH
+    sudo rm -rf /sec/tpm.bootguard_new.txt 
+else
+    read -r -p "DATA GOT TAMPERED, PRESS ENTER TO CONTINUE AND INGORE IT"
+fi
+
+echo "////////////////////////////////////////////////////////////////////////////"
+# Extract the tpm_pcr_8 hashes and compare them to the old txt
+echo "Extract tpm infos"
+sudo tpmtool eventlog dump | grep -E -A 1 -B 2 "initrd /" > ./tpm.kernel_new.txt
+
+if diff -s tpm.kernel_orig.txt tpm.kernel_new.txt 
+then
+    figlet PCR 8 MATCH
+    sudo rm -rf /sec/tpm.kernel_new.txt 
+else
+    read -r -p "DATA GOT TAMPERED, PRESS ENTER TO CONTINUE AND INGORE IT"
+fi
+   
+
+#------------------------------------------Bring up internet and change mac address---------------------------------------------------------------#
+echo "////////////////////////////////////////////////////////////////////////////"
+# Bring up Network Manager
+echo "Bringing up Network Manager"
+sudo systemctl start NetworkManager.service
+sleep 3
+
+# Check if ntwork interface exist, spoof the mac adcflddresses and re-enable the devices
+if test -e /sys/class/net/$ethernet/device; then
+        sudo ifconfig $ethernet down
+        sudo macchanger -r $ethernet
+        sudo ifconfig $ethernet up
+fi
+
+if test -e /sys/class/net/$wireless/device; then
+        sudo ifconfig $wireless down
+        sudo macchanger -r $wireless
+        sudo ifconfig $wireless up 
+fi
+
+#------------------------------------------Update---------------------------------------------------------------#
+echo "////////////////////////////////////////////////////////////////////////////"
+# Ping google until the internet connetion appear
+echo "Waiting for network connection"
+while true; do ping -c1 www.google.com > /dev/null 2>&1 && break; done
+
+echo "////////////////////////////////////////////////////////////////////////////"
+# Update the system
+echo "Updating system"
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install -f -y
+sudo apt-get autoremove --purge -y
+
+#------------------------------------------Recalculate hashes and store on txt---------------------------------------------------------------#
+echo "////////////////////////////////////////////////////////////////////////////"
+# Generate the new sha512 list because some updates can change some values
+echo "Create sha512sum of the /boot partition"
+sudo find /boot -type f -exec sha512sum "{}" + > ./sha512sum_list_boot_orig.txt
+
+echo "////////////////////////////////////////////////////////////////////////////"
+# Generate the new sha512sum because some updates can change some values
+echo "Extract bios infos"
+sudo rm /sec/bios_info_orig.txt
+sudo dmidecode --type bios > ./bios_info_orig.txt
+sudo tail -n +2 '/sec/bios_info_orig.txt' > temp.tmp && sudo mv temp.tmp '/sec/bios_info_orig.txt'
+
+#Generate the new sha512sum because some updates can change some values
+echo "Extract ssd infos"
+sudo rm /sec/ssd_orig.txt
+sudo lshw -class disk | grep /dev/sda -A 5 -B 5 > ./ssd_orig.txt
+
+#Generate the new tpm_pcr_0 because some updates can change some values
+echo "Extract tpm infos"
+sudo tpmtool eventlog dump | grep -A 3 'PCR: 0' > ./tpm.bootguard_orig.txt
+sudo tpmtool eventlog dump | grep -E -A 1 -B 2 "initrd /" > ./tpm.kernel_orig.txt
+
+#------------------------------------------Disable Internet at boot---------------------------------------------------------------#
+# At startup internet will not be up
+nmservice=$(sudo systemctl is-enabled NetworkManager.service)
+
+if [ "$nmservice" == "disabled" ]; then
+    echo "Network Manager service is already disabled"
+else
+    echo "Disabling Network Manager service"
+    sudo systemctl disable NetworkManager.service
+fi
