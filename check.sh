45fabf31562c8639ad78f6def90e4cc02fd695e5
daboynb <106079917+daboynb@users.noreply.github.com>
2022-10-07 14:17:40 +0200

    up

--- check.sh
+++ check.sh
@@ -20,6 +20,7 @@ fi
 
 # Check requirements
 sudo dpkg -l | grep -qw macchanger || sudo apt-get install macchanger -y
+sudo dpkg -l | grep -qw net-tools || sudo apt install net-tools -y
 sudo dpkg -l | grep -qw figlet || sudo apt-get install figlet -y
 sudo dpkg -l | grep -qw dmidecode || sudo apt-get install dmidecode -y
 sudo dpkg -l | grep -qw lshw || sudo apt-get install lshw -y
